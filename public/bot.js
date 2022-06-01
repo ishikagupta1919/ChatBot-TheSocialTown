894d1548dabd7f1062918345c0a4a80b20a19791
ishikagupta1919 <ishikagupta1919@gmail.com>
2022-06-01 17:00:22 +0530

    Uploaded all files

---
+++ public/bot.js
@@ -0,0 +1,15 @@
+// <!-- Start of ChatBot (www.chatbot.com) code -->
+// <script type = "text/javascript" >
+window._be = window._be || {};
+window.__be.id = "60e98a5e73258700077b32a8";
+(function() {
+    var be = document.createElement('script');
+    be.type = 'text/javascript';
+    be.async = true;
+    be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(be, s);
+})();
+console.log("ahi")
+    // </script>
+    // <!-- End of ChatBot code -->
